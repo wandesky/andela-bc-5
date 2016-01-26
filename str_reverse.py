537618347237b01737f67d548921b1e7830e9e71
Waiyaki <muturijames3@gmail.com>
2016-01-26 13:12:25 +0300

    Documented the string reverse function

--- str_reverse.py
+++ str_reverse.py
@@ -1,7 +1,25 @@
 def reverse(s):
+    """
+    Reverse a string using string slicing.
+
+    >>> reverse("Andela")
+    'alednA'
+    >>> reverse("Hello")
+    'olleH'
+    >>> reverse("this is fun")
+    'nuf si siht'
+    >>> reverse("racecar")
+    'racecar'
+    """
     return s[::-1]
 
-# examples
-print reverse("Andela")
-print reverse("Hello")
-print reverse("Philisiah")
+
+def main():
+    print reverse("Andela")
+    print reverse("Hello")
+    print reverse("This is fun.")
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
+    main()
