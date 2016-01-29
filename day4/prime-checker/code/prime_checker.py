686b5c58d1ece6a4ee000ae43a84270790302a8b
James Muturi <Waiyaki@users.noreply.github.com>
2016-01-29 16:47:09 +0300

    Update prime_checker.py

--- day4/prime-checker/code/prime_checker.py
+++ day4/prime-checker/code/prime_checker.py
@@ -14,22 +14,3 @@ class PrimeChecker1(object):
             if arg % i == 0:
                 return False
         return True
-
-
-# class PrimeChecker(object):
-#
-#     def __init__(self, number=''):
-#         self.number = number
-#         if self.number == '':
-#             self.number = 0
-#         else:
-#             self.number = int(self.number)
-#
-#     def is_prime(self):
-#         if self.number < 2:
-#             return False
-#         else:
-#             for i in range(2, self.number + 1):
-#                 if i % 2 == 0:
-#                     return True
-#             return False
